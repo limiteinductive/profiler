497d71b88ace1599181132a4240becf684fb9829
Benjamin Trom <benjamintrom@gmail.com>
2025-03-04 20:23:41 +0100

    Update __init__.py

--- plugin/tensorboard_plugin_profile/convert/__init__.py
+++ plugin/tensorboard_plugin_profile/convert/__init__.py
@@ -13,3 +13,4 @@
 # limitations under the License.
 # ==============================================================================
 """Converter from protobuf to gviz/json format."""
+from tensorflow.python.profiler.internal import _pywrap_profiler_plugin
